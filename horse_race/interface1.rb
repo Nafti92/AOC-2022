6608a9f0ebac5c5eceb431195fc503d3b7a6de25
nafti ladhiri <ladhiri.nafti@gmail.com>
2023-01-18 12:37:57 +0100

    interface horse race

---
+++ horse_race/interface1.rb
@@ -0,0 +1,12 @@
+# Define the horses
+horses = ["Joe the Brave", "Black Stalion", "Ronny the Rat", "Tony Macaroni"]
+
+# Define the initial user balance
+balance = 100
+
+# Display a welcome message
+puts "Welcome to the horse race!"
+puts "--------------------------"
+
+# Start the loop
+# Condition to loop: the user has at least 10 in his balance
